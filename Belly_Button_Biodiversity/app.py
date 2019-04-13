18531a0dd3b5e90f79244112c339c6ab1ac09940
vibrato3 <42424908+vibrato3@users.noreply.github.com>
2019-04-13 14:31:20 -0700

    Add files via upload

---
+++ Belly_Button_Biodiversity/app.py
@@ -0,0 +1,119 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+db = SQLAlchemy(app)
+
+
+Base = automap_base()
+
+Base.prepare(db.engine, reflect=True)
+
+
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
+
+
+@app.route("/")
+def index():
+    
+    return render_template("index.html")
+
+
+@app.route("/names")
+def names():
+    
+
+    
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+   
+    return jsonify(list(df.columns)[2:])
+
+
+@app.route("/metadata/<sample>")
+def sample_metadata(sample):
+  
+    sel = [
+        Samples_Metadata.sample,
+        Samples_Metadata.ETHNICITY,
+        Samples_Metadata.GENDER,
+        Samples_Metadata.AGE,
+        Samples_Metadata.LOCATION,
+        Samples_Metadata.BBTYPE,
+        Samples_Metadata.WFREQ,
+    ]
+
+    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+
+    
+    sample_metadata = {}
+    for result in results:
+        sample_metadata["sample"] = result[0]
+        sample_metadata["ETHNICITY"] = result[1]
+        sample_metadata["GENDER"] = result[2]
+        sample_metadata["AGE"] = result[3]
+        sample_metadata["LOCATION"] = result[4]
+        sample_metadata["BBTYPE"] = result[5]
+        sample_metadata["WFREQ"] = result[6]
+
+    print(sample_metadata)
+    return jsonify(sample_metadata)
+
+
+@app.route("/samples/<sample>")
+def samples(sample):
+    
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+
+    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+    sample_data = sample_data.sort_values(by=[sample] ,ascending=False)
+   
+    data = {
+        "otu_ids": sample_data.otu_id.values.tolist(),
+        "sample_values": sample_data[sample].values.tolist(),
+        "otu_labels": sample_data.otu_label.tolist(),
+    }
+
+    return jsonify(data)
+
+
+@app.route("/wfreq/<sample>")
+def wfreq(sample):
+    selection = [
+    Samples_Metadata.sample,
+    Samples_Metadata.WFREQ,
+    ]
+
+    wfreq_results = db.session.query(*selection).filter(Samples_Metadata.sample == sample).all()
+
+    sample_wfreq = {}
+
+    for result in wfreq_results:
+        sample_wfreq["sample"] = result[0]
+        sample_wfreq["WFREQ"] = result[1]
+
+    return jsonify(sample_wfreq)
+
+
+
+if __name__ == "__main__":
+    app.run()
